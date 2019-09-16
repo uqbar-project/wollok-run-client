b102916b62f7f7d952a4f78c94495246455ce94b
Nicolás Scarcella <nscarcella@gmail.com>
2019-09-16 19:40:48 -0300

    Basic repl layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react'
+import React, { KeyboardEvent, useState } from 'react'
+import CodeEditor from 'react-simple-code-editor'
+import SplitterLayout from 'react-splitter-layout'
+import 'react-splitter-layout/lib/index.css'
 import $ from './App.module.scss'
-import Game from './Game'
+
+type EditorProps = {
+  code: string,
+  onCodeChange: (code: string) => void,
+}
+const Editor = ({ code, onCodeChange }: EditorProps) => {
+  return (
+    <CodeEditor
+      value={code}
+      onValueChange={onCodeChange}
+      placeholder='Write your Wollok object definitions here'
+      highlight={text => text}
+      padding={4}
+      style={{ minHeight: 500 }}
+    />
+  )
+}
+
+
+type ReplProps = {
+  code: string,
+  onCodeChange: (code: string) => void,
+}
+const Repl = ({ code, onCodeChange }: ReplProps) => {
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (event.ctrlKey && event.key === 'Enter') {
+      alert(`Execute: "${code}"`)
+      onCodeChange('')
+    }
+  }
+
+  return (
+    <CodeEditor
+      className={$.repl}
+      value={code}
+      onValueChange={onCodeChange}
+      onKeyDown={onKeyDown}
+      placeholder='Write expressions to evaluate here (ctrl+Enter)'
+      highlight={text => text}
+      padding={4}
+      style={{ minHeight: 200 }}
+    />
+  )
+}
+
 
 const App = () => {
 
-  // const files: File[] = [
-  //   { name: 'src/foo.wlk', content: 'lalalala codigo' },
-  //   { name: 'src/bar.wlk', content: 'lalalala otro codigo' },
-  //   { name: 'src/baz.wlk', content: 'lalalala otro codigo más' },
-  // ]
+  const [editorCode, setEditorCode] = useState(localStorage.getItem('editor-code')!)
+  const onEditorCodeChange = (code: string) => {
+    localStorage.setItem('editor-code', code)
+    setEditorCode(code)
+  }
 
-  // const [selectedFile, setSelectedFile] = useState<File | null>(null)
-
-  // return (
-  //   <div className={styles.app}>
-  //     <SourceView files={files} setSelectedFile={setSelectedFile} selectedFile={selectedFile} />
-  //   </div>
-  // )
+  const [replCode, setReplCode] = useState('')
 
   return (
     <div className={$.app}>
-      <Game />
+      <SplitterLayout vertical percentage customClassName={$.workspace} secondaryInitialSize={20} >
+        <SplitterLayout percentage>
+          <Editor code={editorCode} onCodeChange={onEditorCodeChange} />
+          <div>DIAGRAM ZONE</div>
+        </SplitterLayout>
+
+        <Repl code={replCode} onCodeChange={setReplCode} />
+      </SplitterLayout>
+      <div className={$.statusBar}>Status: OK</div>
     </div>
   )
 }
