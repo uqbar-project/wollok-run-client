258ffbaef09ed382b0b09750616f6cb402b9ed3d
Nicolás Scarcella <nscarcella@gmail.com>
2019-09-20 12:44:14 -0300

    Removing App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
+import { Redirect, Router } from '@reach/router'
+import whyDidYouRender from '@welldone-software/why-did-you-render'
+import 'rc-tooltip/assets/bootstrap_white.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import 'react-splitter-layout/lib/index.css'
+import Game from './components/Game'
+import Worksheet from './components/worksheet/Worksheet'
 import './index.scss'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+if (process.env.NODE_ENV !== 'production') whyDidYouRender(React)
+
+const Routes = () => (
+  <Router>
+    <Worksheet path='/worksheet' />
+    <Game path='/game' />
+    <Redirect from='/' to='/worksheet' default noThrow />
+  </Router>
+)
+
+ReactDOM.render(<Routes />, document.getElementById('root'))
 
 serviceWorker.unregister()
