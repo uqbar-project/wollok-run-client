92dd2f2fd5e18ddd687d71ad699a7e369ce12940
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2020-11-12 10:46:59 -0300

    better bubble size

--- src/components/game/messages.ts
+++ src/components/game/messages.ts
@@ -12,21 +12,21 @@ function messageSizeLimit() {
   return { x: sizeFactor * 3, y: sizeFactor * 3 }
 }
 
-function messageTextPosition(message: DrawableMessage) {
-  return { x: message.x + sizeFactor, y: message.y - sizeFactor / 2 }
+function messageTextPosition(sketch: p5, message: DrawableMessage) {
+  return { x: message.x + sizeFactor + 5, y: message.y - messageSize(sketch, message).y * 1.05 }
 }
 
 function messageSize(sketch: p5, message: DrawableMessage) {
   const sizeLimit = messageSizeLimit()
   const textWidth = sketch.textWidth(message.message)
   const xSize = Math.min(textWidth, sizeLimit.x)
-  const ySize = sizeFactor * Math.ceil(textWidth / sizeLimit.y) / 2
+  const ySize = (sizeFactor - 15) * Math.ceil(textWidth / sizeLimit.x) / 2
   return { x: xSize, y: ySize }
 }
 
 function messageBackgroundPosition(sketch: p5, message: DrawableMessage) {
-  const xPosition = message.x + sizeFactor - 5
-  const yPosition = messageTextPosition(message).y - sketch.textDescent()
+  const xPosition = message.x + sizeFactor
+  const yPosition = messageTextPosition(sketch, message).y
   return { x: xPosition, y: yPosition }
 }
 
@@ -38,7 +38,7 @@ function drawMessageBackground(sketch: p5, message: DrawableMessage) {
 
 export const drawMessage = (sketch: p5) => (message: DrawableMessage): void => {
   drawMessageBackground(sketch, message)
-  const position = messageTextPosition(message)
+  const position = messageTextPosition(sketch, message)
   const limit = messageSizeLimit()
   sketch.text(message.message, position.x, position.y, limit.x, limit.y)
 }
