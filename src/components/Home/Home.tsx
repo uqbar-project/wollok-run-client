7d7773d522b495c28bc0c2e525d597520a58c626
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2020-11-25 12:41:02 -0300

    vertical home buttons

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core'
+import { Button, ButtonGroup } from '@material-ui/core'
 import { Link, RouteComponentProps } from '@reach/router'
 import React from 'react'
 import $ from './Home.module.scss'
@@ -10,10 +10,16 @@ export const WollokLogo = ({ }: RouteComponentProps) => {
 export const Home = ({ }: RouteComponentProps) => {
   return <div className={$.container}>
     <WollokLogo />
-    <div>
-      <Button component={Link} to={'/game'} variant="contained" color="primary">Game</Button>
-      <Button component={Link} to={'/worksheet'} variant="contained" color="primary">Worksheet</Button>
-      <Button component={Link} to={'/debugger'} variant="contained" color="primary">Debugger</Button>
+    <div className={$.buttons}>
+      <ButtonGroup
+        size="large"
+        orientation="vertical"
+        variant="contained"
+        color="primary">
+        <Button component={Link} to={'/game'} variant="contained" color="primary">Game</Button>
+        <Button component={Link} to={'/worksheet'} variant="contained" color="primary">Worksheet</Button>
+        <Button component={Link} to={'/debugger'} variant="contained" color="primary" >Debugger</Button>
+      </ButtonGroup>
     </div>
   </div>
 }
