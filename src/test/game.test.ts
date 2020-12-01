e94131cedd7a71f1f9cb51bd384fe2c1482caf19
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2020-12-01 14:32:39 -0300

    removed board layers functions

--- src/test/game.test.ts
+++ src/test/game.test.ts
@@ -14,34 +14,6 @@ const readFiles = (files: string[]) => files.map(file => ({
 }))
 
 describe('game', () => {
-  const ground1 = { img: 'ground1' }
-  const ground2 = { img: 'ground2' }
-  const pepita2 = { img: 'pepita2' }
-  const pepita3 = { img: 'pepita3' }
-
-  const board = [
-    [[ground1], [ground1, pepita2], [ground1]],
-    [[ground2], [ground2, pepita2, pepita3], [ground2]],
-  ]
-
-  test('board to layers', () => {
-    const layers = boardToLayers(board)
-    expect(layers).toEqual([
-      [
-        [ground1, ground1, ground1],
-        [ground2, ground2, ground2],
-      ],
-      [
-        [undefined, pepita2, undefined],
-        [undefined, pepita2, undefined],
-      ],
-      [
-        undefined,
-        [undefined, pepita3, undefined],
-      ],
-    ])
-  })
-
   const gameTest = (testName: string, gameProgramFile: string, gameFiles: string[], cbTest: (evaluation: Evaluation) => void) => {
     const environment = buildEnvironment(readFiles(gameFiles))
     const { buildEvaluation, runProgram } = interpret(environment, wre)
