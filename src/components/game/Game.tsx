f018aa1d20718cec4c6fa129783e373744ae952c
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2020-12-15 15:50:47 -0300

    readme button

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -7,8 +7,9 @@ import wre from 'wollok-ts/dist/wre/wre.natives'
 import FilesSelector, { File } from '../filesSelector/FilesSelector'
 import Sketch from './Sketch'
 import $ from './Game.module.scss'
-import { gameInstance } from './GameStates'
 import { GameProject, buildGameProject, mainProgram } from './gameProject'
+import { IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const natives = wre as Natives
 
@@ -16,6 +17,11 @@ export type GameProps = RouteComponentProps
 const Game = (_: GameProps) => {
   const [game, setGame] = useState<GameProject>()
   const [evaluation, setEvaluation] = useState<Evaluation>()
+  const [showReadme, setShowReadme] = useState<boolean>(false)
+  const handleDrawerOpen = () => {
+    setShowReadme(!showReadme)
+  }
+
 
   const loadGame = (files: File[]) => {
     const project = buildGameProject(files)
@@ -27,15 +33,14 @@ const Game = (_: GameProps) => {
     setEvaluation(cleanEval)
   }
 
-  const title = evaluation ? evaluation.instance(gameInstance(evaluation).get('title')!.id).innerValue : ''
-
   return !evaluation || !game
     ? <FilesSelector onFilesLoad={loadGame} />
     : <div className={$.container}>
-      <h1>{title}</h1>
       <div>
         <Sketch game={game} evaluation={evaluation} />
-        <ReactMarkdown source={game.description} className={$.description} />
+        <div><IconButton color='primary' onClick={handleDrawerOpen}> <MenuIcon /> </IconButton></div>
+        {showReadme && (<ReactMarkdown source={game.description} className={$.description} />)}
+
       </div>
     </div>
 }
