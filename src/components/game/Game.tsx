3566831222700374b11740cf1d3c125bc10b7c43
Ezequiel Pereyra <31800576+ezequielPereyra@users.noreply.github.com>
2021-09-07 03:58:07 -0300

    Can select program to run if a project has a few

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -6,20 +6,21 @@ import interpret from 'wollok-ts/dist/interpreter/interpreter'
 import FilesSelector, { File } from '../filesSelector/FilesSelector'
 import Sketch from './Sketch'
 import $ from './Game.module.scss'
-import { GameProject, buildGameProject, getProgramIn } from './gameProject'
+import { GameProject, buildGameProject, getProgramIn, TooManyProgramsException } from './gameProject'
 import { WollokLogo } from '../Home/Home'
 import { CenterFocusStrong } from '@material-ui/icons'
 import { BackArrow } from '../BackArrow'
+import { Button, FormControl, NativeSelect } from '@material-ui/core'
 
 export type GameProps = RouteComponentProps
 const Game = (_: GameProps) => {
   const [game, setGame] = useState<GameProject>()
   const [evaluation, setEvaluation] = useState<Evaluation>()
-  const [error, setError] = useState<string>()
+  const [error, setError] = useState<Error>()
 
-  const loadGame = (files: File[]) => {
+  const loadGame = (files: File[], program?: string) => {
     try {
-      const project = buildGameProject(files)
+      const project = buildGameProject(files, program)
       const environment = buildEnvironment(project.sources)
       const interpreter = interpret(environment, WRENatives)
       interpreter.exec(getProgramIn(project.main, environment))
@@ -28,7 +29,7 @@ const Game = (_: GameProps) => {
     }
     catch(exception){
       if(exception instanceof Error){
-        setError(exception.message)
+        setError(exception)
       }
     }
   }
@@ -36,16 +37,7 @@ const Game = (_: GameProps) => {
   const title = evaluation ? evaluation.object('wollok.game.game')?.get('title')?.innerValue : ''
 
   if(error){
-    return  <div className={$.error}>
-              <WollokLogo />
-              <div>
-                <h1> {error} </h1>
-                <p style={{marginTop:'5px', marginBottom:'5px'}}>No se encontro un programa, cuya extension es .wpgm, dentro del proyecto.
-                  Podes crear uno dentro de la carpeta src para poder correr el juego.
-                </p>
-                <BackArrow returnPath='/'/> 
-              </div>
-            </div>
+    return  cantLoadProgramView(error)
   }
   if(!evaluation || !game)
     return <FilesSelector onFilesLoad={loadGame} />
@@ -58,17 +50,40 @@ const Game = (_: GameProps) => {
             </div>
           </div>
 
+  function cantLoadProgramView(error: Error) {
 
-
-  // return !evaluation || !game
-  //   ? <FilesSelector onFilesLoad={loadGame} />
-  //   : <div className={$.container}>
-  //     <h1>{title}</h1>
-  //     <div>
-  //       <Sketch gameProject={game} evaluation={evaluation} />
-  //       <ReactMarkdown source={game.description} className={$.description} />
-  //     </div>
-  //   </div>
+    if(error instanceof TooManyProgramsException){
+      let program: string
+      return <div className={$.error}>
+      <WollokLogo />
+      <div>
+        <h1> {error.message} </h1>
+        <p style={{ marginTop: '5px', marginBottom: '5px' }}>Se encontraron varios programas en el proyecto.
+         Seleccione uno para continuar:
+        </p>
+        <FormControl className = {$.wpgmPicker}>
+          <NativeSelect defaultValue = "" style={{padding: '1%'}} onChange={(e) => program = e.target.value}>
+          <option value="" disabled>Seleccione el programa para correr el juego</option>
+            {error.wpgmFiles.map((file, i) => <option key={i} value={file.name}>{file.name}</option>)}
+          </NativeSelect>
+        </FormControl>
+        <BackArrow returnPath='/' />
+        <Button onClick={() => { setError(undefined); loadGame(error.files, program) }} variant="contained" color="primary">Cargar Juego</Button>
+      </div>
+    </div>
+    }
+  
+    return <div className={$.error}>
+      <WollokLogo />
+      <div>
+        <h1> {error.message} </h1>
+        <p style={{ marginTop: '5px', marginBottom: '5px' }}>No se encontro un programa, cuya extension es .wpgm, dentro del proyecto.
+          Podes crear uno dentro de la carpeta src para poder correr el juego.
+        </p>
+        <BackArrow returnPath='/' />
+      </div>
+    </div>
+  }
 }
 
 export default memo(Game)
